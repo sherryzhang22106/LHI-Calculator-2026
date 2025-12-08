903bc89a4066e1bbc716f7c9097928c590dd325b
sherryzhang22106 <sherryzhang22106@gmail.com>
2025-12-08 17:59:41 +0800

    Setup PostgreSQL migrations for Vercel deployment
    - Create PostgreSQL-compatible migration files
    - Add vercel-build script to run migrations on deployment
    - Update migration_lock.toml to use postgresql provider

--- server/prisma/migrations/20251205072115_add_ai_analysis/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "assessments" ADD COLUMN "aiAnalysis" TEXT;
--- server/prisma/migrations/20251205082715_allow_multiple_assessments_per_code/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "assessments_accessCodeId_key";
-
--- CreateIndex
-CREATE INDEX "assessments_accessCodeId_idx" ON "assessments"("accessCodeId");
